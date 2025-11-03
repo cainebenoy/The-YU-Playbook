b8e50c8e5d5b685f4248c28b6026fbbce8ae9ae9
CAINE BENOY <cainebenoy@gmail.com>
2025-11-03 16:37:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-67602257-2ea9c",
+  "appId": "1:579253323409:web:ace9ab11aef3fc87df6130",
+  "apiKey": "AIzaSyDkEqs-zOEGriiLOw2EXdVcbZ8T6Oa0860",
+  "authDomain": "studio-67602257-2ea9c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "579253323409"
+};
